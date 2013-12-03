34fa799f6d23c8520c3fc689644aae44bcae332d
Mark Moissette <kaosat.dev@gmail.com>
2013-12-03 15:11:04 +0100

    - basic demo is working !  * loading files using xhr store + stl parser works  * added demo stl files (to get arround cross domain issues (depends on http server))  * various experimens with related polymer elements

--- element/asset-manager.js
+++ element/asset-manager.js
@@ -1,25 +1,23 @@
 Polymer('asset-manager', {
   created: function() {
     AssetManager = require("AssetManager");
-    console.log("AssetManager",AssetManager);
     this._assetManager = new AssetManager();
+
+    //stores, parsers, assetcache
   },
-  ready:function(){
-  },
-  enteredView: function() {
+  _onResourceLoaded: function( resource) 
+  {
+    this.fire("resource:loaded",resource);
   },
   //API
-  addParser: function( extension, parser ): //why would we want to add them on the fly via api ?
+  addParser: function( extension, parser ) //why would we want to add them on the fly via api ?
   {
     return this._assetManager.addParser( extension, parser );
   },
-  load: function( fileUri, parentUri, cachingParams )
+  read: function( fileUri, parentUri, cachingParams )
   {
-    //return this._assetManager.load( fileUri, parentUri, cachingParams );
-    var promise = this._assetManager._testDeferred( fileUri );
-    promise.then(function(result){
-
-      console.log("Promise resolved with value", result);
-    });
+    var promise = this._assetManager.load( fileUri, parentUri, cachingParams );
+    promise.then( this._onResourceLoaded.bind(this) );
+    return promise
   }
 });
